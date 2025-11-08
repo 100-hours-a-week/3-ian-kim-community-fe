87ecc8e8acadba8ee5741c8706d7648205560067
gnsdp <rladpgns99@gmail.com>
2025-11-09 01:35:09 +0900

    feat: 게시글 목록 페이지 구현

---
+++ src/pages/post/post-list/post-card.js
@@ -0,0 +1,45 @@
+import { formatCompactNumber } from "../../../utils/fomat-utils.js";
+
+export default class PostCard {
+  #target;
+  #post;
+
+  constructor({ target, post }) {
+    this.#target = target;
+    this.#post = post;
+    this.#render();
+  }
+
+  #render() {
+    this.#target.insertAdjacentHTML("beforeend", this.#template());
+  }
+
+  #template() {
+    return /*html*/ `
+      <article class="post-card" data-post-id=${this.#post.postId}>
+        <h3 class="post-title">${this.#post.title.slice(0, 26)}</h3>
+        <div class="post-info">
+          <div class="post-stats">
+            <span class="like-cnt"
+              >좋아요 ${formatCompactNumber(this.#post.likeCount)}</span
+            >
+            <span class="comment-cnt"
+              >댓글 ${formatCompactNumber(this.#post.commentCount)}</span
+            >
+            <span class="view-cnt"
+              >조회수 ${formatCompactNumber(this.#post.viewCount)}</span
+            >
+          </div>
+          <span class="created-date">${this.#post.createdDate}</span>
+        </div>
+
+        <hr class="divider" />
+
+        <div class="author-profile">
+          <img src="" alt="작성자 프로필" class="author-profile-icon bg-gray" />
+          <span class="author-name">${this.#post.author}</span>
+        </div>
+      </article>
+    `;
+  }
+}
