b9f5f5c3aa5f0f86486f6d19c12087514b2f7c87
gnsdp <rladpgns99@gmail.com>
2025-11-09 01:35:09 +0900

    feat: 게시글 목록 페이지 구현

---
+++ src/utils/fomat-utils.js
@@ -0,0 +1,15 @@
+export const formatCompactNumber = (number) => {
+  if (number >= 100_000) {
+    return "100k";
+  }
+
+  if (number >= 10_000) {
+    return "10k";
+  }
+
+  if (number >= 1_000) {
+    return "1k";
+  }
+
+  return "" + number;
+};
