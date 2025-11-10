7c3ac678a657dbb9ca3b9d72faabfd21a06a8e29
gnsdp <rladpgns99@gmail.com>
2025-11-10 17:13:56 +0900

    feat: 회원가입 페이지 수정

--- src/pages/user/register/register.js
+++ src/pages/user/register/register.js
@@ -5,6 +5,7 @@ import {
   register,
 } from "../../../api/user-api.js";
 import { MESSAGES } from "../../../common/constants.js";
+import Component from "../../../Component.js";
 import Header from "../../../components/header/Header.js";
 import { navigateTo, ROUTES } from "../../../router/router.js";
 import {
@@ -23,60 +24,49 @@ import {
   profileImageValidator,
 } from "../../../utils/validation-utils.js";
 
-export default class Register {
-  #VALIDATORS = {
-    email: emailValidator,
-    password: passwordValidator,
-    confirm: (value, inputs) =>
-      passwordConfirmValidator(inputs.password.value, value),
-    nickname: nicknameValidator,
-    // image: profileImageValidator,
-  };
-  #inputs = {};
-  #helperTexts = {};
-  #profilePreview;
-  #profileImage;
-  #registerBtn;
-  #loginLink;
-
-  constructor() {
-    this.#render();
+export default class Register extends Component {
+  setUp() {
+    this.VALIDATORS = {
+      email: emailValidator,
+      password: passwordValidator,
+      confirm: (value, inputs) =>
+        passwordConfirmValidator(inputs.password.value, value),
+      nickname: nicknameValidator,
+      // image: profileImageValidator,
+    };
+    this.inputs = {};
+    this.helperTexts = {};
   }
-
-  #render() {
+  afterMounted() {
     new Header(document.querySelector(".header"), {
       hasBackBtn: true,
     });
-    this.#selectElements();
-    this.#addEvents();
+
+    setInputElemets(this.inputs, this.helperTexts, this.VALIDATORS);
+    this.profilePreview = document.querySelector(".profile-preview");
+    this.profileImage = document.querySelector(".profile-image");
+    this.registerBtn = document.querySelector(".btn-register");
+    this.loginLink = document.querySelector(".link-login");
   }
 
-  #selectElements() {
-    setInputElemets(this.#inputs, this.#helperTexts, this.#VALIDATORS);
-    this.#profilePreview = document.querySelector(".profile-preview");
-    this.#profileImage = document.querySelector(".profile-image");
-    this.#registerBtn = document.querySelector(".btn-register");
-    this.#loginLink = document.querySelector(".link-login");
-  }
-
-  #addEvents() {
+  setEvents() {
     // 입력값 검증
     addValidationEvents(
-      this.#inputs,
-      this.#helperTexts,
-      this.#registerBtn,
-      this.#VALIDATORS
+      this.inputs,
+      this.helperTexts,
+      this.registerBtn,
+      this.VALIDATORS
     );
 
-    this.#inputs.password.addEventListener("blur", (e) => {
-      this.#helperTexts.confirm.textContent = this.#VALIDATORS.confirm(
-        this.#inputs.confirm.value,
-        this.#inputs
+    this.inputs.password.addEventListener("blur", (e) => {
+      this.helperTexts.confirm.textContent = this.VALIDATORS.confirm(
+        this.inputs.confirm.value,
+        this.inputs
       );
-      checkAllInputValid(this.#inputs, this.#helperTexts, this.#registerBtn);
+      checkAllInputValid(this.inputs, this.helperTexts, this.registerBtn);
     });
 
-    this.#inputs.email.addEventListener("blur", async (e) => {
+    this.inputs.email.addEventListener("blur", async (e) => {
       const email = e.target.value;
 
       if (emailValidator(email)) {
@@ -87,11 +77,11 @@ export default class Register {
       const data = await parseData(response);
 
       if (isSuccess(response) && !data.available) {
-        this.#helperTexts.email.textContent = MESSAGES.duplicatedEmail;
+        this.helperTexts.email.textContent = MESSAGES.duplicatedEmail;
       }
     });
 
-    this.#inputs.nickname.addEventListener("blur", async (e) => {
+    this.inputs.nickname.addEventListener("blur", async (e) => {
       const nickname = e.target.value;
 
       if (nicknameValidator(nickname)) {
@@ -102,38 +92,126 @@ export default class Register {
       const data = await parseData(response);
 
       if (isSuccess(response) && !data.available) {
-        this.#helperTexts.nickname.textContent = MESSAGES.duplicatedNickname;
+        this.helperTexts.nickname.textContent = MESSAGES.duplicatedNickname;
       }
     });
 
     // todo: 이미지 업로드
     // addUploadProfileImageEvent(
-    //   this.#inputs,
-    //   this.#helperTexts,
-    //   this.#profilePreview,
-    //   this.#profileImage,
-    //   this.#registerBtn
+    //   this.inputs,
+    //   this.helperTexts,
+    //   this.profilePreview,
+    //   this.profileImage,
+    //   this.registerBtn
     // );
 
     // 회원가입 API 요청
-    this.#registerBtn.addEventListener("click", async () => {
-      if (!isButtonEnabled(this.#registerBtn)) {
+    this.registerBtn.addEventListener("click", async () => {
+      if (!isButtonEnabled(this.registerBtn)) {
         return;
       }
 
-      const response = await register(parseInputValues(this.#inputs));
+      const response = await register(parseInputValues(this.inputs));
 
       if (isSuccess(response)) {
         alert("회원가입에 성공했습니다.");
         navigateTo(ROUTES.LOGIN);
+        return;
       }
+
+      alert("회원가입에 실패했습니다.");
     });
 
     // 페이지 이동
-    this.#loginLink.addEventListener("click", () => {
+    this.loginLink.addEventListener("click", () => {
       navigateTo(ROUTES.LOGIN);
     });
   }
+
+  template() {
+    return /*html*/ `
+      <h1>회원가입</h1>
+
+      <form class="user-form">
+        <div class="user-form-input-group register-form-input-group">
+          <label for="image" class="label">프로필 사진 *</label>
+
+          <p class="helper-text helper-text-image text-red">
+            *프로필 사진을 추가해주세요.
+          </p>
+
+          <div class="profile-preview bg-gray">
+            <span class="plus-icon">+</span>
+            <img class="profile-image hidden" />
+          </div>
+          <input
+            type="file"
+            id="image"
+            class="input-image hidden"
+            accept="image/*"
+            autocomplete="off" />
+        </div>
+
+        <div class="user-form-input-group register-form-input-group">
+          <label for="email" class="label">이메일 *</label>
+          <input
+            type="email"
+            id="email"
+            class="input-email"
+            autocomplete="off"
+            placeholder="이메일을 입력하세요." />
+
+          <p class="helper-text helper-text-email text-red"></p>
+        </div>
+
+        <div class="user-form-input-group register-form-input-group">
+          <label for="password" class="label">비밀번호 *</label>
+          <input
+            type="password"
+            id="password"
+            class="input-password"
+            autocomplete="off"
+            placeholder="비밀번호를 입력하세요." />
+
+          <p class="helper-text helper-text-password text-red"></p>
+        </div>
+
+        <div class="user-form-input-group register-form-input-group">
+          <label for="confirm" class="label">비밀번호 확인*</label>
+          <input
+            type="password"
+            id="confirm"
+            class="input-confirm"
+            autocomplete="off"
+            placeholder="비밀번호를 한번 더 입력하세요." />
+
+          <p class="helper-text helper-text-confirm text-red"></p>
+        </div>
+
+        <div class="user-form-input-group register-form-input-group">
+          <label for="nickname" class="label">닉네임*</label>
+          <input
+            type="text"
+            id="nickname"
+            class="input-nickname"
+            autocomplete="off"
+            placeholder="닉네임을 입력하세요." />
+
+          <p class="helper-text helper-text-nickname text-red"></p>
+        </div>
+
+        <button
+          type="button"
+          class="btn-register user-form-button bg-btn-disabled text-white">
+          회원가입
+        </button>
+      </form>
+
+      <a href="#" class="link-login user-form-link text-black"
+        >로그인하러 가기</a
+      >
+    `;
+  }
 }
 
-new Register();
+new Register(document.querySelector(".container"));
