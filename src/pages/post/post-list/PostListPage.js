072256789b24af0aa672e5b0ebb0fdeab44aac66
gnsdp <rladpgns99@gmail.com>
2025-11-10 21:21:02 +0900

    feat: 게시글 목록 페이지 무한스크롤 구현

--- src/pages/post/post-list/PostListPage.js
+++ src/pages/post/post-list/PostListPage.js
@@ -1,5 +1,5 @@
 import Component from "../../../Component.js";
-import { parseData } from "../../../api/base-api.js";
+import { isSuccess, parseData } from "../../../api/base-api.js";
 import { getPostList } from "../../../api/post-api.js";
 import Header from "../../../components/header/Header.js";
 import PostCard from "../../../components/post/PostCard.js";
@@ -14,6 +14,9 @@ export default class PostListPage extends Component {
 
     this.page = 0;
     this.hasNextPage = false;
+    this.$lastPostCard;
+    this.$postList;
+    this.posts = [];
   }
 
   async afterRendered() {
@@ -21,23 +24,67 @@ export default class PostListPage extends Component {
       hasProfileIcon: true,
     });
 
-    // todo: 무한스크롤 구현
-    const response = await getPostList(this.page);
-    const data = await parseData(response);
+    this.$postList = document.querySelector(".post-list");
+    this.observer = new IntersectionObserver(
+      (entries) => this.handleObserve(entries),
+      {
+        root: this.$postList,
+        rootMargin: "0px",
+        scrollMargin: "0px",
+        threshold: 0.5,
+      }
+    );
 
-    this.hasNextPage = data.page.number < data.page.totalPages;
-
-    data.content.forEach((post) => {
-      new PostCard(document.querySelector(".post-list"), {
-        post,
-      });
-    });
+    await this.handleGetPostList(this.$postList);
   }
 
   setEvents() {
     document.querySelector(".btn-write").addEventListener("click", () => {
       navigateTo(ROUTES.POST_CREATE);
     });
+  }
+
+  handleObserve(entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && this.hasNextPage) {
+        this.handleGetPostList();
+      }
+    });
+  }
+
+  async handleGetPostList() {
+    const response = await getPostList(this.page++);
+
+    if (!isSuccess(response)) {
+      alert("게시글 목록 조회에 실패했습니다.");
+      return;
+    }
+
+    const data = await parseData(response);
+    if (!data?.content) {
+      return;
+    }
+
+    this.posts = data.content;
+    this.hasNextPage = data.page.number < data.page.totalPages;
+    this.addPostCards();
+  }
+
+  addPostCards() {
+    this.posts.forEach((post) => {
+      new PostCard(this.$postList, {
+        post,
+      });
+    });
+
+    const $nextPostCard = this.$postList.querySelector(".post-card:last-child");
+    if ($nextPostCard) {
+      if (this.$lastPostCard) {
+        this.observer.unobserve(this.$lastPostCard);
+      }
+      this.$lastPostCard = $nextPostCard;
+      this.observer.observe(this.$lastPostCard);
+    }
   }
 
   template() {
