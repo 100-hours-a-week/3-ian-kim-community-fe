3955fb3647603bd6fb0648c3086a6a1a77a03309
gnsdp <rladpgns99@gmail.com>
2025-11-10 17:09:25 +0900

    feat: 로그인 페이지 수정

--- src/pages/user/login/login.js
+++ src/pages/user/login/login.js
@@ -1,6 +1,7 @@
 import { isSuccess, parseData } from "../../../api/base-api.js";
 import { login } from "../../../api/user-api.js";
 import { MESSAGES } from "../../../common/constants.js";
+import Component from "../../../Component.js";
 import Header from "../../../components/header/Header.js";
 import { navigateTo, ROUTES } from "../../../router/router.js";
 import { Auth } from "../../../store/auth-store.js";
@@ -15,64 +16,101 @@ import {
   passwordValidator,
 } from "../../../utils/validation-utils.js";
 
-export default class Login {
-  #VALIDATORS = {
-    email: emailValidator,
-    password: passwordValidator,
-  };
-  #inputs = {};
-  #helperTexts = {};
-  #loginBtn;
-  #registerLink;
+export default class Login extends Component {
+  setUp() {
+    this.VALIDATORS = {
+      email: emailValidator,
+      password: passwordValidator,
+    };
 
-  constructor() {
-    this.#render();
+    this.$inputs = {};
+    this.$helperTexts = {};
   }
 
-  #render() {
+  afterMounted() {
     new Header(document.querySelector(".header"), {});
-    this.#selectElements();
-    this.#addEvents();
+
+    setInputElemets(this.$inputs, this.$helperTexts, this.VALIDATORS);
+    this.$loginBtn = document.querySelector(".btn-login");
+    this.$registerLink = document.querySelector(".link-register");
   }
 
-  #selectElements() {
-    setInputElemets(this.#inputs, this.#helperTexts, this.#VALIDATORS);
-    this.#loginBtn = document.querySelector(".btn-login");
-    this.#registerLink = document.querySelector(".link-register");
-  }
-
-  #addEvents() {
+  setEvents() {
     // 입력값 검증
     addValidationEvents(
-      this.#inputs,
-      this.#helperTexts,
-      this.#loginBtn,
-      this.#VALIDATORS
+      this.$inputs,
+      this.$helperTexts,
+      this.$loginBtn,
+      this.VALIDATORS
     );
 
     // 로그인 API 요청
-    this.#loginBtn.addEventListener("click", async () => {
-      if (!isButtonEnabled(this.#loginBtn)) {
+    this.$loginBtn.addEventListener("click", async () => {
+      if (!isButtonEnabled(this.$loginBtn)) {
         return;
       }
 
-      const response = await login(parseInputValues(this.#inputs));
+      const response = await login(parseInputValues(this.$inputs));
       const data = await parseData(response);
 
       if (isSuccess(response)) {
         alert("로그인에 성공했습니다.");
         Auth.login(data.userId);
         navigateTo(ROUTES.POST_LIST);
+        return;
       }
 
-      this.#helperTexts.email.textContent = MESSAGES.wrongEmailOrPassword;
+      if (response.status === 401) {
+        this.$helperTexts.email.textContent = MESSAGES.wrongEmailOrPassword;
+        return;
+      }
+
+      alert("로그인에 실패했습니다.");
     });
 
     // 페이지 이동
-    this.#registerLink.addEventListener("click", () => {
+    this.$registerLink.addEventListener("click", () => {
       navigateTo(ROUTES.REGISTER);
     });
   }
+
+  template() {
+    return /*html*/ `
+      <h1>로그인</h1>
+
+      <form class="user-form">
+        <div class="user-form-input-group login-form-input-group">
+          <label for="email" class="label">이메일</label>
+          <input
+            type="text"
+            id="email"
+            class="input-email"
+            placeholder="이메일을 입력하세요." />
+        </div>
+
+        <div class="user-form-input-group login-form-input-group">
+          <label for="password" class="label">비밀번호</label>
+          <input
+            type="password"
+            id="password"
+            class="input-password"
+            autocomplete="off"
+            placeholder="비밀번호를 입력하세요." />
+        </div>
+
+        <p
+          class="helper-text helper-text-email helper-text-password text-red"></p>
+
+        <button
+          type="button"
+          class="btn-login user-form-button bg-btn-disabled text-white">
+          로그인
+        </button>
+      </form>
+
+      <a href="#" class="link-register user-form-link text-black">회원가입</a>
+    `;
+  }
 }
 
-new Login();
+new Login(document.querySelector(".container"));
