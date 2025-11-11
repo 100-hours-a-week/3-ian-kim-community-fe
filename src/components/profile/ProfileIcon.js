842be6cb21f70267cc44f404ad27ae41e6f2c12a
gnsdp <rladpgns99@gmail.com>
2025-11-12 08:42:23 +0900

    feat: 회원 프로필 아이콘 컴포넌트 구현

---
+++ src/components/profile/ProfileIcon.js
@@ -0,0 +1,20 @@
+import Component from "../../Component.js";
+import { getUserProfile } from "../../utils/image-utils.js";
+
+export default class ProfileIcon extends Component {
+  beforeRendered() {
+    this.id = this.props.id;
+    this.profilePath = this.props.profilePath;
+  }
+
+  async afterRendered() {
+    this.$profileIcon = document.querySelector(`#${this.id}`);
+    if (this.profilePath) {
+      this.$profileIcon.src = await getUserProfile(this.profilePath);
+    }
+  }
+
+  template() {
+    return /*html*/ ` <img src="" alt="프로필" id=${this.id} class="profile-icon bg-gray" /> `;
+  }
+}
