287d8d2768ab33add93e4769ac394cb2d7b536af
gnsdp <rladpgns99@gmail.com>
2025-11-15 19:41:53 +0900

    feat: 댓글 페이지네이션 구현

--- src/components/comment/CommentSection.js
+++ src/components/comment/CommentSection.js
@@ -3,6 +3,9 @@ import { deleteComment } from "../../api/comment-api.js";
 import Component from "../core/Component.js";
 import CommentCreate from "./CommentCreate.js";
 import CommentItem from "./CommentItem.js";
+import CommentPage from "./CommentPage.js";
+
+const COMMENTS_PER_PAGE = 5;
 
 export default class CommentSection extends Component {
   beforeRendered() {
@@ -11,16 +14,29 @@ export default class CommentSection extends Component {
   }
 
   afterRendered() {
-    this.commentCreate = new CommentCreate(
-      document.querySelector(".comment-create"),
-      {
-        postId: this.postId,
-      }
-    );
+    this.commentCreate = new CommentCreate(document.querySelector(".comment-create"), {
+      postId: this.postId,
+    });
 
-    const $commentList = document.querySelector(".comment-list");
-    this.comments.forEach((comment) => {
-      new CommentItem($commentList, {
+    this.$commentList = document.querySelector(".comment-list");
+    this.$commentPagination = document.querySelector(".comment-pagination");
+
+    this.loadCommentsOnPage(1);
+
+    new CommentPage(this.$commentPagination, {
+      totalPageCnt: Math.max(1, Math.ceil(this.comments.length / COMMENTS_PER_PAGE)),
+      onPageChange: (page) => this.loadCommentsOnPage(page),
+    });
+  }
+
+  loadCommentsOnPage(page) {
+    this.currentPage = page;
+    this.$commentList.innerHTML = "";
+    const start = (this.currentPage - 1) * COMMENTS_PER_PAGE;
+    const end = start + COMMENTS_PER_PAGE;
+
+    this.comments.slice(start, end).forEach((comment) => {
+      new CommentItem(this.$commentList, {
         comment,
         onClickEdit: (comment) => this.handleCommentEdit(comment),
         onDelete: (commentId) => this.handleCommentDelete(commentId),
@@ -50,10 +66,14 @@ export default class CommentSection extends Component {
         <span class="comment-stat-name">답변</span>
         <span class="comment-cnt">${this.comments.length}</span>
       </div>
+
       <div class="comment-create"></div>
+
       <ul class="comment-list">
         <li class="comment-item"></li>
       </ul>
+
+      <div class="comment-pagination"></div>
     `;
   }
 }
