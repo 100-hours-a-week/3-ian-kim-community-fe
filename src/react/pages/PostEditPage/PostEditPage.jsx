aafd4eb418aef568065321902edbf6a6d27c0a00
gnsdp <rladpgns99@gmail.com>
2025-11-27 13:50:29 +0900

    feat: 게시글 수정 페이지 구현

--- src/react/pages/PostEditPage/PostEditPage.jsx
+++ src/react/pages/PostEditPage/PostEditPage.jsx
@@ -1,5 +1,22 @@
+import PostForm from '@/components/form/PostForm.jsx'
+import useImageUpload from '@/hooks/useImageUpload.jsx'
+import useInput from '@/hooks/useInput.jsx'
+import { Validators } from '@/utils/validation.js'
+
 function PostEditPage() {
-  return <></>
+  const titleInput = useInput('', Validators.postTitle)
+  const contentInput = useInput('', Validators.postContent)
+  const imageInput = useImageUpload(null)
+
+  const handleEditClick = () => {
+    // TODO: 게시글 수정 API 연결
+  }
+
+  return (
+    <>
+      <PostForm inputs={[titleInput, contentInput, imageInput]} headerText={'질문 수정'} buttonText={'수정하기'} onButtonClick={handleEditClick} />
+    </>
+  )
 }
 
 export default PostEditPage
