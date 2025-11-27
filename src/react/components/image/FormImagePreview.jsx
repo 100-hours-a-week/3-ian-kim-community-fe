0ad177213e3a5998104c94b0ec6de48857518172
gnsdp <rladpgns99@gmail.com>
2025-11-27 11:50:35 +0900

    feat: FormImagePreview 컴포넌트 생성

---
+++ src/react/components/image/FormImagePreview.jsx
@@ -0,0 +1,17 @@
+import styles from '@/components/image/FormImagePreview.module.css'
+
+function FormImagePreview({ onImageClick, imgSrc, imgName }) {
+  return (
+    <>
+      <div className={styles['image-group']}>
+        <div className={styles['image-preview']} onClick={onImageClick}>
+          <img src={imgSrc} className={`${styles['image']} ${imgSrc || 'hidden'}`} />
+          <span className={styles['edit-text']}>+</span>
+        </div>
+        <span className={styles['image-name']}>{imgName}</span>
+      </div>
+    </>
+  )
+}
+
+export default FormImagePreview
