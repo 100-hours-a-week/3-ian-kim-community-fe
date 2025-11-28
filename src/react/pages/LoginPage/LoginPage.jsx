e6175fdbfac34a8250c4770ef8da9d18b7c13598
gnsdp <rladpgns99@gmail.com>
2025-11-28 14:56:52 +0900

    feat: 로그인 API 연동

--- src/react/pages/LoginPage/LoginPage.jsx
+++ src/react/pages/LoginPage/LoginPage.jsx
@@ -6,32 +6,40 @@ import { ROUTES } from '@/routes/routes.js'
 import { checkInputsValid, Validators } from '@/utils/validation.js'
 import { useNavigate } from 'react-router'
 import Form from '@/components/form/Form.jsx'
+import { loginUser } from '@/api/user.js'
+import LoginRequest from '@/api/dto/request/LoginRequest.js'
 
 function LoginPage() {
   const navigate = useNavigate()
 
-  const email = useInput('', Validators.email)
-  const password = useInput('', Validators.password)
+  const emailInput = useInput('', Validators.email)
+  const passwordInput = useInput('', Validators.password)
 
-  const inputsValid = checkInputsValid([email, password])
+  const inputsValid = checkInputsValid([emailInput, passwordInput])
 
   const handleLinkClick = () => {
     navigate(ROUTES.REGISTER)
   }
 
-  const handleButtonClick = () => {
-    // TODO: 로그인 API 연결
+  const handleButtonClick = async () => {
+    try {
+      const response = await loginUser(new LoginRequest({ email: emailInput.value, password: passwordInput.value }))
+      // TODO: 로그인 상태 관리
+
+      alert('로그인에 성공했습니다.')
+      navigate(ROUTES.HOME)
+    } catch (e) {}
   }
 
   return (
     <>
-      <Form headerText={'로그인'} buttonText={'로그인'} onButtonClick={handleButtonClick} inputs={[email, password]} inputsValid={inputsValid}>
+      <Form headerText={'로그인'} buttonText={'로그인'} onButtonClick={handleButtonClick} inputs={[emailInput, passwordInput]} inputsValid={inputsValid}>
         <FormInputGroup labelText={'이메일'} id={'email'}>
-          <FormInput type={'text'} id={'email'} placeholder={'이메일을 입력하세요.'} value={email.value} onChangeInput={email.onChange} />
+          <FormInput type={'text'} id={'email'} placeholder={'이메일을 입력하세요.'} value={emailInput.value} onChangeInput={emailInput.onChange} />
         </FormInputGroup>
 
         <FormInputGroup labelText={'비밀번호'} id={'password'}>
-          <FormInput type={'password'} id={'password'} placeholder={'비밀번호를 입력하세요.'} value={password.value} onChangeInput={password.onChange} />
+          <FormInput type={'password'} id={'password'} placeholder={'비밀번호를 입력하세요.'} value={passwordInput.value} onChangeInput={passwordInput.onChange} />
         </FormInputGroup>
       </Form>
 
