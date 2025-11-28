59757d0d8953f5098896fb9c7f39925b074a58b9
gnsdp <rladpgns99@gmail.com>
2025-11-28 09:39:49 +0900

    feat: 무한스크롤 커스텀 훅 생성

---
+++ src/react/hooks/useInfiniteScroll.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from 'react'
+
+const useInfiniteScroll = (callback) => {
+  const hasNextPage = useRef(false)
+  const targetRef = useRef(null)
+
+  useEffect(() => {
+    if (!targetRef.current) {
+      return
+    }
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && hasNextPage.current) {
+          callback()
+        }
+      },
+      {
+        threshold: 1.0,
+      },
+    )
+
+    observer.observe(targetRef.current)
+
+    return () => observer.disconnect()
+  }, [targetRef.current])
+
+  const updateHasNextPage = (page) => {
+    hasNextPage.current = page.number < page.totalPages
+  }
+
+  return { targetRef, updateHasNextPage }
+}
+
+export default useInfiniteScroll
