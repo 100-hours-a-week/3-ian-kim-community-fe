e5feade8dde8a64e60cd6a021ad838480c15f037
gnsdp <rladpgns99@gmail.com>
2025-11-28 16:34:35 +0900

    feat: 로그아웃 API 연동

--- src/react/components/header/HeaderProfile.jsx
+++ src/react/components/header/HeaderProfile.jsx
@@ -5,12 +5,17 @@ import { ROUTES } from '@/routes/routes.js'
 import Button from '@/components/button/Button.jsx'
 import ProfileIcon from '@/components/profile/ProfileIcon.jsx'
 import { getImage } from '@/api/image.js'
+import { logoutUser } from '@/api/user.js'
+import { useAuthStore } from '@/stores/authStore.js'
+import { ERROR_MESSAGE } from '@/api/error.js'
 
 function HeaderProfile({ profileImageName }) {
   const navigate = useNavigate()
 
   const [profileImage, setProfileImage] = useState(null)
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+  const resetUser = useAuthStore((store) => store.resetUser)
 
   useEffect(() => {
     const getProfileImage = async () => {
@@ -36,16 +41,23 @@ function HeaderProfile({ profileImageName }) {
     setIsDropdownOpen((prev) => !prev)
   }
 
-  const handleAccountEditBtnClick = () => {
+  const handleAccountEditClick = () => {
     navigate(ROUTES.MY_EDIT_ACCOUNT)
   }
 
-  const handlePasswordEditBtnClick = () => {
+  const handlePasswordEditClick = () => {
     navigate(ROUTES.MY_EDIT_PASSWORD)
   }
 
-  const handleLogoutBtnClick = () => {
-    // TODO: 로그아웃 API 연결
+  const handleLogoutClick = async () => {
+    try {
+      await logoutUser()
+      alert('로그아웃에 성공했습니다.')
+      resetUser(null)
+      navigate(ROUTES.LOGIN)
+    } catch ({ code }) {
+      alert(ERROR_MESSAGE[code])
+    }
   }
 
   return (
@@ -53,9 +65,9 @@ function HeaderProfile({ profileImageName }) {
       <ProfileIcon image={profileImage} size={'2rem'} cursor={'pointer'} onIconClick={handleIconClick} />
 
       <div className={`${styles['profile-dropdown']} ${isDropdownOpen || 'hidden'}`}>
-        <Button text={'회원정보수정'} className={styles['profile-dropdown-btn']} onButtonClick={handleAccountEditBtnClick} />
-        <Button text={'비밀번호수정'} className={styles['profile-dropdown-btn']} onButtonClick={handlePasswordEditBtnClick} />
-        <Button text={'로그아웃'} className={styles['profile-dropdown-btn']} onButtonClick={handleLogoutBtnClick} />
+        <Button text={'회원정보수정'} className={styles['profile-dropdown-btn']} onButtonClick={handleAccountEditClick} />
+        <Button text={'비밀번호수정'} className={styles['profile-dropdown-btn']} onButtonClick={handlePasswordEditClick} />
+        <Button text={'로그아웃'} className={styles['profile-dropdown-btn']} onButtonClick={handleLogoutClick} />
       </div>
     </div>
   )
